d4db021907f2026e2fd0efcf7405e264d35ef049
Rafael Mendonça França <rafaelmfranca@gmail.com>
2010-05-09 18:56:08 -0300

    Adicionada a rota para root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Secomp::Application.routes.draw do |map|
+  get "articles/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ Secomp::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "articles#index"
 
   # See how all your routes lay out with "rake routes"
 
