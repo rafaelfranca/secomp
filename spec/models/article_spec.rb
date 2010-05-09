520fd18a56e467322dd3fe228a5d2b450ce40d46
Rafael Mendonça França <rafaelmfranca@gmail.com>
2010-05-09 19:08:00 -0300

    Adicionada validações nos artigos

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,6 +1,18 @@
 require 'spec_helper'
 
 describe Article do
-  it "deveria ter um titulo"
-  it "deveria ter um texto"
+  it "deveria ser valido com atributos validos" do
+    article = Factory.build(:article)
+    article.should be_valid
+  end
+
+  it "deveria ter um titulo" do
+    article = Factory.build(:article, :title => nil)
+    article.should_not be_valid
+  end
+
+  it "deveria ter um texto" do
+    article = Factory.build(:article, :body => nil)
+    article.should_not be_valid
+  end
 end
