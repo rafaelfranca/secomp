b6994a6e9f3e59b543581f3bbbb68ec4b4b652ff
Rafael Mendonça França <rafaelmfranca@gmail.com>
2010-05-09 19:41:28 -0300

    Corrigida a mensagem de teste

--- spec/views/articles/index.html.erb_spec.rb
+++ spec/views/articles/index.html.erb_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "articles/index.html.erb" do
-  it "deveria conter o link para adicionar um novo" do
+  it "deveria conter o link para adicionar um novo artigo" do
     render
     response.should have_selector("a", :href => new_article_path)
   end
